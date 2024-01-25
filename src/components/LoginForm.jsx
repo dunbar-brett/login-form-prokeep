96a078c3c8ff58f94a93eea92faecb50b7563041
dunbar-brett <dunbar.brett@gmail.com>
2024-01-24 17:58:20 -0800

    finalized most of the css for login form. add some base level functionality to inputs and links

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,33 +1,72 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { FaLock, FaUser } from "react-icons/fa";
 import './LoginForm.css';
 
 const LoginForm = () => {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleUsernameChange = (e) => {
+        // handle validation
+        setUsername(e.target.value);
+    };
+
+    const handlePasswordChange = (e) => {
+        // handle validation
+        setPassword(e.target.value);
+    };
+
+    const handleRememberMeChange = (e) => {
+        console.log('Remember me checked -- not implemented');
+    };
+
+    const handleForgotPasswordClick = (e) => {
+        e.preventDefault();
+        console.log('Forgot password link clicked -- not implemented');
+    };
+
+    const handleRegisterClick = (e) => {
+        e.preventDefault();
+        console.log('Register link clicked -- not implemented');
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(`Username: ${username} Password: ${password}`);
+        // add logic
+    };
+
     return (
         <div className='wrapper'>
-            <form action="">
+            <form onSubmit={handleSubmit}>
                 <h1>Login</h1>
                 <div className='input-box'>
-                    <input type="text" placeholder='Username' required/>
+                    <input type="text"
+                        placeholder='Username'
+                        onChange={handleUsernameChange}
+                        required/>
                     <FaUser className='icon' />
                 </div>
                 <div className='input-box'>
-                    <input type="password" placeholder='Password' required/>
+                    <input type="password"
+                        placeholder='Password'
+                        onChange={handlePasswordChange}
+                        required/>
                     <FaLock className='icon' />
                 </div>
 
                 <div className="remember-me">
                     <label>
-                        <input type="checkbox" />
+                        <input type="checkbox" onChange={handleRememberMeChange}/>
                         Remember me
                     </label>
-                    <a href="#">Forgot password?</a>
+                    <a href="#" onClick={handleForgotPasswordClick}>Forgot password?</a>
                 </div>
 
                 <button type="submit">Login</button>
 
                 <div className="register-link">
-                    <p>Don't have an account? <a href="#">Register</a></p>
+                    <p>Don't have an account? <a href="#" onClick={handleRegisterClick}>Register</a></p>
                 </div>
             </form>
         </div>
